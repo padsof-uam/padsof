710319fbbf15b5c73b6970eeae05198336e48a43
Guillermo Julián <gjulian93@gmail.com>
2013-03-08 12:03:01 +0100

    Merge resolved

--- PADSOF/Padsof/src/padsof/bookings/BookingFactory.java
+++ PADSOF/Padsof/src/padsof/bookings/BookingFactory.java
@@ -1,6 +1,7 @@
 package padsof.bookings;
 
 import java.util.*;
+import padsof.bookings.*;
 import padsof.services.*;
 import padsof.system.*;
 
@@ -8,11 +9,25 @@ public class BookingFactory
 {
 	public Booking book(Service service, Client client, Date start, Date end)
 	{
-		// TODO: Mirar bien cómo hacerlo.
-		return null;
+		if(Flight.class.isInstance(service))
+			return createFlightBooking((Flight)service, client, start, end);
+		else if(Hotel.class.isInstance(service))
+			return createHotelBooking((Hotel)service, client, start, end);
+		else if(ImsersoTravel.class.isInstance(service))
+			return createImsersoTravelBooking(service, client, start, end);
+		else if(Travel.class.isInstance(service))
+			return createTravelBooking(service, client, start, end);
+		else
+			throw new UnsupportedOperationException("Class " + service.getClass().getName() + " doesn't have a booking constructor.");
 	}
+	
 	private Booking createFligthBooking(Flight service, Client client, Date start, Date end){
 		FlightBooking FB = new FlightBooking(service,client,start,end);
 		return FB;				
+	}
+
+	private Booking createHotelBooking(Hotel service, Client client, Date start, Date end)
+	{
+		
 	}
 }
