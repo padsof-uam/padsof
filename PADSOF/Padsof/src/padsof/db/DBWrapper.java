e95da1ea44c1ad5a1ad00cf97a354c54f53f9f27
Guillermo Julián <gjulian93@gmail.com>
2013-03-20 17:28:18 +0100

    Changed DBWrapper with singleton model.

--- PADSOF/Padsof/src/padsof/db/DBWrapper.java
+++ PADSOF/Padsof/src/padsof/db/DBWrapper.java
@@ -3,6 +3,7 @@
  */
 package padsof.db;
 
+import java.io.File;
 import java.sql.SQLException;
 
 import java.util.HashMap;
@@ -24,70 +25,81 @@ public class DBWrapper
 
 	public static DBWrapper getInstance() throws SQLException
 	{
-		if(instance == null)
-			instance = new DBWrapper("productionDB");
-		
+		if (instance == null)
+			new DBWrapper("productionDB");
+
 		return instance;
 	}
-	
+
 	private HashMap<String, Dao<?, Long>> daos = new HashMap<String, Dao<?, Long>>();
 	private ConnectionSource dataSource = null;
-	
+	private String dbFile;
+
 	/**
 	 * Constructor. Public for test purposes.
+	 * 
 	 * @param dbName
 	 * @throws SQLException
 	 */
 	public DBWrapper(String dbName) throws SQLException
 	{
-		dataSource = new JdbcConnectionSource("jdbc:sqlite:" + dbName + ".sqlite");
+		dbFile = dbName + ".sqlite";
+		dataSource = new JdbcConnectionSource("jdbc:sqlite:" + dbFile);
+		instance = this;
 	}
-	
-	private Dao<?, ?> getDaoFor(Class<?> cls) throws SQLException
+
+	private Dao<?, Long> getDaoFor(Class<?> cls) throws SQLException
 	{
-		if(daos.containsKey(cls.getName()))
+		if (daos.containsKey(cls.getName()))
 			return daos.get(cls.getName());
-		
+
 		Dao<?, Long> dao = DaoManager.createDao(dataSource, cls);
 		daos.put(cls.getName(), dao);
-		
+
 		TableUtils.createTableIfNotExists(dataSource, cls);
-		
+
 		return dao;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public <T> CreateOrUpdateStatus save(T item) throws SQLException, IllegalArgumentException, IllegalAccessException
-	{				
-		Dao<T, ?> dao = (Dao<T, ?>) getDaoFor(item.getClass());		
-		
+	public <T> CreateOrUpdateStatus save(T item) throws SQLException,
+			IllegalArgumentException, IllegalAccessException
+	{
+		Dao<T, ?> dao = (Dao<T, ?>) getDaoFor(item.getClass());
+
 		return dao.createOrUpdate(item);
 	}
 
 	public void close() throws SQLException
 	{
-		if(dataSource != null)
+		if (dataSource != null)
 			dataSource.close();
 	}
-	
+
+	public void delete()
+	{
+		File f = new File(dbFile);
+		f.delete();
+	}
+
 	@SuppressWarnings("unchecked")
 	public <T> List<T> getAll(Class<T> cls) throws SQLException
 	{
 		Dao<T, Long> dao = (Dao<T, Long>) getDaoFor(cls);
-		
+
 		return dao.queryForAll();
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public <T> List<T> get(Class<T> cls, Object...values) throws SQLException
-	{	
+	public <T> List<T> get(Class<T> cls, Object... values) throws SQLException
+	{
 		HashMap<String, Object> fieldValues = new HashMap<String, Object>();
-		
-		for(int i = 0; i < values.length - 1; i+= 2)
-			fieldValues.put((String)values[i], values[i + 1]);
-		
+
+		for (int i = 0; i < values.length - 1; i += 2)
+			fieldValues.put((String) values[i], values[i + 1]);
+
 		Dao<T, Long> dao = (Dao<T, Long>) getDaoFor(cls);
-		return dao.queryForFieldValues(fieldValues);		
+		return dao.queryForFieldValues(fieldValues);
 	}
 
 	public void clear(Class<?> cls) throws SQLException
@@ -101,16 +113,17 @@ public class DBWrapper
 		Dao<T, Long> dao = (Dao<T, Long>) getDaoFor(item.getClass());
 		dao.delete(item);
 	}
-	
+
 	public void clear() throws ClassNotFoundException, SQLException
 	{
-		for(String cls : daos.keySet())
+		for (String cls : daos.keySet())
 			clear(Class.forName(cls));
 	}
-	
-	public <T> void save(Iterable<T> items) throws SQLException, IllegalArgumentException, IllegalAccessException
+
+	public <T> void save(Iterable<T> items) throws SQLException,
+			IllegalArgumentException, IllegalAccessException
 	{
-		for(T item : items)
+		for (T item : items)
 			save(item);
 	}
 }
