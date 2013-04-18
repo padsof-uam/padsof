bf2ab895c5aab4a362f032ecdd5424284c4bfae5
Victor <vicdejuan@Vicdejuan.(none)>
2013-04-18 12:47:09 +0200

    vendor

--- PADSOF/Padsof/src/padsof/gui/views/MainView.java
+++ PADSOF/Padsof/src/padsof/gui/views/MainView.java
@@ -51,7 +51,7 @@ public class MainView
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
-		View pene = new FindPacketView();
+		View pene = new VendorFirstView();
 		pene.show(frame);
 		
 	
