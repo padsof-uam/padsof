c95c76d1f9792844181b304abae9ea222f608df9
Guillermo Julián <gjulian93@gmail.com>
2013-04-18 23:40:36 +0200

    Unused import.

--- PADSOF/Padsof/src/padsof/gui/controllers/LoginController.java
+++ PADSOF/Padsof/src/padsof/gui/controllers/LoginController.java
@@ -2,7 +2,6 @@ package padsof.gui.controllers;
 
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
-import java.sql.SQLException;
 import java.util.List;
 
 import javax.swing.JOptionPane;
