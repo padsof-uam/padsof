ccba7cd0bbda2510304e71d6aa3c4f361622473b
Guillermo Julián <gjulian93@gmail.com>
2013-04-20 16:15:30 +0200

    Documented.

--- PADSOF/Padsof/src/padsof/gui/controllers/Controller.java
+++ PADSOF/Padsof/src/padsof/gui/controllers/Controller.java
@@ -7,24 +7,66 @@ import padsof.gui.NavigationService;
 import padsof.gui.controllers.utils.Listener;
 import padsof.gui.views.View;
 
-public class Controller<V extends View> implements ActionListener
+/**
+ * Controller base class.
+ * This class allows to set up easily listeners to events. Just annotate the
+ * method with Listener and, optionally, the command to which that method
+ * responds to. Don't forget to set the action command in the performer.
+ * For example, to listen to a button press, set this method on the controller:
+ * 
+ * <pre>
+ * <code>
+ * @Listener("method")
+ * public void listener(ActionEvent e)
+ * {
+ * 		// ...
+ * }
+ * </code>
+ * </pre>
+ * 
+ * and this on the View, setting the button:
+ * 
+ * <pre>
+ * {@code
+ * button.setActionCommand("method");
+ * button.addActionListener(controller);
+ * }
+ * </pre>
+ * 
+ * The ActionEvent argument is optional on the listener, you don't have to
+ * include it.
+ * 
+ * @author Víctor de Juan Sanz - Guillermo Julián Moreno
+ * @param <V>
+ *            View controlled by the controller.
+ */
+public abstract class Controller<V extends View> implements ActionListener
 {
 	protected V view;
 	protected NavigationService navigator;
-	
+
 	/**
-	 * @param navigator the navigator to set
+	 * @param navigator
+	 *            the navigation service.
 	 */
 	public void setNavigator(NavigationService navigator)
 	{
 		this.navigator = navigator;
 	}
 
+	/**
+	 * Link the view to the controller.
+	 * 
+	 * @param view
+	 */
 	public void setView(V view)
 	{
 		this.view = view;
 	}
 
+	/**
+	 * Listens to action performed and calls the corresponding listener method.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
@@ -34,7 +76,7 @@ public class Controller<V extends View> implements ActionListener
 		{
 			if (listener != null)
 			{
-				if(methodReceivesArgs(listener))
+				if (methodReceivesArgs(listener))
 					listener.invoke(this, e);
 				else
 					listener.invoke(this);
@@ -59,14 +101,16 @@ public class Controller<V extends View> implements ActionListener
 		{
 			if (method.isAnnotationPresent(Listener.class))
 			{
-				if(!hasListenerSignature(method))
+				if (!hasListenerSignature(method))
 				{
-					System.out.println("Warning: method " + method.getName() + " doesn't have listener signature.");
+					System.out.println("Warning: method " + method.getName()
+							+ " doesn't have listener signature.");
 					continue;
 				}
-				
+
 				Listener annotation = method.getAnnotation(Listener.class);
-				if (command.equals(annotation.value()) || annotation.value().equals("__SINGLE__"))
+				if (command.equals(annotation.value())
+						|| annotation.value().equals("__SINGLE__"))
 					return method;
 			}
 		}
@@ -77,7 +121,9 @@ public class Controller<V extends View> implements ActionListener
 	private boolean hasListenerSignature(Method method)
 	{
 		Class<?>[] params = method.getParameterTypes();
-		
-		return params.length == 0 || (params.length == 1 && params[0].isAssignableFrom(ActionEvent.class));
+
+		return params.length == 0
+				|| (params.length == 1 && params[0]
+						.isAssignableFrom(ActionEvent.class));
 	}
 }
